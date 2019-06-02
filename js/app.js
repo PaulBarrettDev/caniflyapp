69f40dea4d28bec4cc2ebf1e8a2dcbbffb4f7288
Paul Barrett <paulbarrett@Pauls-MacBook-Pro.local>
2019-06-02 11:37:05 +0100

    Cleaned up the JS file

--- js/app.js
+++ js/app.js
@@ -1,55 +1,58 @@
+// --------------------------------------------------------------------------------------------
+// Listen for page load
+// --------------------------------------------------------------------------------------------
 window.addEventListener("load", () => {
+  // --------------------------------------------------------------------------------------------
+  // Variables
+  // --------------------------------------------------------------------------------------------
   let long;
   let lat;
   const temperatureDescription = document.querySelector(".temperature-description");
   const temperatureDegree = document.querySelector(".temperature-degree");
   const locationTimezone = document.querySelector(".location-timezone");
-  const temperatureSection = document.querySelector(".temperature");
-  const temperatureSpan = document.querySelector(".temperature span");
   const windspeedSection = document.querySelector(".windspeed");
+  // --------------------------------------------------------------------------------------------
   // Get location information from users browser
+  // --------------------------------------------------------------------------------------------
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-
+      // --------------------------------------------------------------------------------------------
       // CORS Error resolution for local host
+      // --------------------------------------------------------------------------------------------
       const proxy = "http://cors-anywhere.herokuapp.com/";
-
+      // --------------------------------------------------------------------------------------------
       // Darksky API
+      // --------------------------------------------------------------------------------------------
       const api = `${proxy}https://api.darksky.net/forecast/b18bd104f4ae8d4a532c3df0a4a9a0bb/${lat},${long}`;
-
+      // --------------------------------------------------------------------------------------------
       // Fetch JSON Data
+      // --------------------------------------------------------------------------------------------
       fetch(api)
         .then(response => {
           return response.json();
         })
+        // --------------------------------------------------------------------------------------------
+        // set DOM elements from API
+        // --------------------------------------------------------------------------------------------
         .then(data => {
           const { temperature, summary, icon, windSpeed } = data.currently;
-          // set DOM elements from API
           temperatureDegree.textContent = `Current temperature is ${Math.floor((temperature - 32) * (5 / 9))}`;
           temperatureDescription.textContent = `Today is ${summary}`;
           locationTimezone.textContent = data.timezone;
-          windspeedSection.textContent = `Todays wind speed is ${windSpeed} mph`;
-          // Convert Farenheight to Celcius
-          const celsius = (temperature - 32) * (5 / 9);
-          // Set Icon
+          windspeedSection.textContent = `Todays wind speed is ${Math.floor(windSpeed)} mph`;
+          // --------------------------------------------------------------------------------------------
+          // Set Skycon Icon
+          // --------------------------------------------------------------------------------------------
           setIcons(icon, document.querySelector(".icon"));
-
-          // Change Temperature To Celcius
-          temperatureSection.addEventListener("click", () => {
-            if (temperatureSpan.textContent === "F") {
-              temperatureSpan.textContent = "C";
-              temperatureDegree.textContent = Math.floor(celsius);
-            } else {
-              temperatureSpan.textContent = "F";
-              temperatureDegree.textContent = temperature;
-            }
-          });
         });
     });
   }
-  // skycons
+
+  // --------------------------------------------------------------------------------------------
+  // Add Skycons
+  // --------------------------------------------------------------------------------------------
   function setIcons(icon, iconID) {
     const skycons = new Skycons({ color: "white" });
     const currentIcon = icon.replace(/-/g, "_").toUpperCase();
